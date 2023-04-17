2ddc44737cf490c229ed13af0db5b16540f8b1c1
Ryan Setaruddin <coolry1998@gmail.com>
2023-04-17 12:35:21 -0400

    work in-progress

--- src/game-journal-fe/src/App.js
+++ src/game-journal-fe/src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
-function GameList({listOfGames}) {
+function GameList() {
+
+  const [listOfGames, setlistOfGames] = useState(Array(listOfGames).fill(null));
 
   let gameObjects = listOfGames.map((gameName) => (<Game name={gameName}></Game>));
+
+  
 
   return <div className="gamelist">{gameObjects}</div>;
 }
@@ -20,8 +24,6 @@ function App() {
     "Candy Crush Sage", "Dota2", "Grand Theft Auto V", "Pac-Man", "Resident Evil 4", "Mario Bros.", 
     "Mario Kart 8 Deluxe", "Super Mario Odyssey", "Sper Smash Bros. Ultimate", "Fall Guys", "Overwatch", 
     "Overwatch 2", "Call of duty: Modern Warfare 2", "The Legends of Zelda: Breath of the Wild"];
-
-  // const [game, setGame] = useState(Array(listOfGames).fill(null));
 
   return (
     <div className="populargames">
