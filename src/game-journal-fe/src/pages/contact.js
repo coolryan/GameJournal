60d8298e79ccc02520b862d7332e628e932d2a32
Ryan Setaruddin <coolry1998@gmail.com>
2023-05-18 19:33:03 -0400

    add content to contact page. However having issue with css and error 404- Not Found for contact.

--- src/game-journal-fe/src/pages/contact.js
+++ src/game-journal-fe/src/pages/contact.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const FORM_ENDPOINT = "";
   
 const Contact = () => {
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+
+    return (
+      <div>
+        <div className="thankYouText">Thank You!</div>
+        <div className="keepIntouchSoonText">We'll be in touch soon.</div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-     
-    </div>
+    <form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST" target="_blank">
+      <div className="contactForm">
+        <input type="text" placeholder="Your name" name="name" className="inputName" required />
+      </div>
+      <div className="contactForm">
+        <input type="email" placeholder="Email" name="email" className="inputEmail" required />
+      </div>
+      <div className="contactForm">
+        <textarea placeholder="Your message" name="message" className="inputMessage" required />
+      </div>
+      <div className="contactForm">
+        <button className="submitbtn" type="submit">
+          Send a message
+        </button>
+      </div>
+    </form>
   );
 };
   
