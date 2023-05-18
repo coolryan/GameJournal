c1639b3714cf7f463cc14156074ca8b92174a800
Ryan Setaruddin <coolry1998@gmail.com>
2023-05-18 18:37:36 -0400

    fixed

--- src/game-journal-fe/src/pages/contact.js
+++ src/game-journal-fe/src/pages/contact.js
@@ -3,21 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-     <form id="contact-form" /*onSubmit={this.handleSubmit.bind(this)}*/ method="POST">
-       <div className="form-goup">
-         <label>Name</label>
-         <input type="text" className="form-control" />
-       </div>
-       <div className="form-goup">
-         <label>Email</label>
-         <input type="email" className="form-control" aria-describedby=""emailhelp/>
-       </div>
-       <div className="form-goup">
-         <label>Message</label>
-         <textarea className="form-control" rows="5"></textarea>
-       </div>
-       <button type="submit" className="btn btn-primary">Submit</button>
-     </form>
+     
     </div>
   );
 };
