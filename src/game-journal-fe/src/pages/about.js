3901c860ad863436540f2a46e827c60653872c81
Ryan Setaruddin <coolry1998@gmail.com>
2023-05-22 17:59:46 -0400

    Fixed up About page CSS. add LinkedIn link and talk about deploying. Next add more details in about page and add endpoint for contact page and we will setup AWS account in secure way to deploy our preoject.

--- src/game-journal-fe/src/pages/about.js
+++ src/game-journal-fe/src/pages/about.js
@@ -7,8 +7,18 @@ const About = () => {
   		<p>
   			Game Journal is about content creators and gamers to upload and review their favortie video games on online.<br/>
   		</p>
+      <br/>
+      <p>
+        My name is Ryan Setaruddin and am entry-level software developer.
+        I graduated and earned Bachlors of Science in Computer systems and information from Farmingdale State College.
+        I learned how to be programmer and now am looking for programming job in New York.
+        I have done few group projects from Farmingdale State College like e.g: Align web application, collaborative with five other classmates on a mentor web app to connect student mentors and student mentees with each other along with helpful resources.
+      </p>
       <button className="linkbtn">
-        <a href="https://github.com/coolryan/GameJournal">Click Me!</a>
+        <a href="https://github.com/coolryan/GameJournal">View Source Code</a>
+      </button>
+      <button className="linkbtn">
+        <a href="https://www.linkedin.com/in/ryan-setaruddin/">View<img src="https://static.vecteezy.com/system/resources/previews/017/339/624/original/linkedin-icon-free-png.png" alt="LinkedIn"/></a>
       </button>
     </div>
   );
